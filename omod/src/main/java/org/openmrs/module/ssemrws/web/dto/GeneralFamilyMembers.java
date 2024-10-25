c4e604dcaba9e8b6637fb23522653dfc68f02c1e
Your Name <michaelndula@gmail.com>
2024-10-25 15:41:30 +0300

    Removing manual getters and setters to use automatically generated getter and setters using annotations.

--- omod/src/main/java/org/openmrs/module/ssemrws/web/dto/GeneralFamilyMembers.java
+++ omod/src/main/java/org/openmrs/module/ssemrws/web/dto/GeneralFamilyMembers.java
@@ -1,5 +1,8 @@
 package org.openmrs.module.ssemrws.web.dto;
 
+import lombok.Data;
+
+@Data
 public class GeneralFamilyMembers {
 	
 	private String familyMemberName;
@@ -9,36 +12,4 @@ public class GeneralFamilyMembers {
 	private String familyMemberAge;
 	
 	private String familyMemberSex;
-	
-	public void setFamilyMemberName(String familyMemberName) {
-		this.familyMemberName = familyMemberName;
-	}
-	
-	public String getFamilyMemberName() {
-		return familyMemberName;
-	}
-	
-	public void setFamilyMemberRelationship(String familyMemberRelationship) {
-		this.familyMemberRelationship = familyMemberRelationship;
-	}
-	
-	public String getFamilyMemberRelationship() {
-		return familyMemberRelationship;
-	}
-	
-	public void setFamilyMemberAge(String familyMemberAge) {
-		this.familyMemberAge = familyMemberAge;
-	}
-	
-	public String getFamilyMemberAge() {
-		return familyMemberAge;
-	}
-	
-	public void setFamilyMemberSex(String familyMemberSex) {
-		this.familyMemberSex = familyMemberSex;
-	}
-	
-	public String getFamilyMemberSex() {
-		return familyMemberSex;
-	}
 }
