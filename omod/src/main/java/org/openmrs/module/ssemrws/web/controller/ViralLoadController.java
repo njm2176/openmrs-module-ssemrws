5e0bac9f8c331b17687e1b03528dc435120b9b49
Munyua123 <earlmunyua96@gmail.com>
2025-07-28 04:54:11 +0300

    SSEMR-641 Transfer the calculation for the cart data to be done in the backend to reduce the loading time

--- omod/src/main/java/org/openmrs/module/ssemrws/web/controller/ViralLoadController.java
+++ omod/src/main/java/org/openmrs/module/ssemrws/web/controller/ViralLoadController.java
@@ -298,6 +298,47 @@ public class ViralLoadController {
 	}
 	
 	/**
+	 * Handles the calculation for the viral load suppression chart in the hiv art module using data
+	 * from existing functions
+	 */
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/chart/viralLoadSuppression")
+	@ResponseBody
+	public Object viralLoadSuppressionChart(HttpServletRequest request, @RequestParam("startDate") String qStartDate,
+	        @RequestParam("endDate") String qendDate,
+	        @RequestParam(required = false, value = "filter") SSEMRWebServicesController.filterCategory filterCategory,
+	        @RequestParam(value = "page", required = false) Integer page,
+	        @RequestParam(value = "size", required = false) Integer size) throws ParseException {
+		SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date[] dates = getStartAndEndDate(qStartDate, qendDate, dateTimeFormatter);
+		
+		int totalPatients = Context.getPatientService().getAllPatients().size();
+		
+		if (totalPatients == 0) {
+			Map<String, Integer> response = new HashMap<>();
+			response.put("totalPatients", totalPatients);
+			response.put("vlCoverage", 0);
+			response.put("vlSuppressed", 0);
+			
+			return response;
+		}
+		
+		List<Patient> vlCoveredPatients = fetchPatientsWithViralLoadCoverage(dates[0], dates[1]);
+		int vlCoverage = vlCoveredPatients.size();
+		int vlSuppressed = countViralLoadSuppressedPatients(vlCoveredPatients, dates[0], dates[1]);
+		
+		int suppressed = vlCoverage > 0 ? Math.round(((float) vlSuppressed / vlCoverage) * 100) : 0;
+		int unSuppressed = vlCoverage > 0 ? 100 - suppressed : 0;
+		
+		Map<String, Integer> response = new HashMap<>();
+		response.put("Total Patients", totalPatients);
+		response.put("suppressed", suppressed);
+		response.put("UnSuppressed", unSuppressed);
+		
+		return response;
+	}
+
+	/**
 	 * Get count of VL Suppressed Patients (BDL or VL < 1000)
 	 */
 	private int countViralLoadSuppressedPatients(List<Patient> vlCoveredPatients, Date startDate, Date endDate) {
